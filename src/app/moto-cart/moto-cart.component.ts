c5961399e90d69e81d42be7a3b7463d1ed79d20a
Maria Dolores Parral <doloresparral@hotmail.com>
2022-11-26 19:47:21 -0300

    modifico el carrito, agrego vaciarlo

--- src/app/moto-cart/moto-cart.component.ts
+++ src/app/moto-cart/moto-cart.component.ts
@@ -10,27 +10,33 @@ import { MotosListComponent } from '../motos-list/motos-list.component';
   templateUrl: './moto-cart.component.html',
   styleUrls: ['./moto-cart.component.scss']
 })
-export class MotoCartComponent implements OnInit {
- 
-  cartList!: Moto [];
-  constructor(private cart:MotoCartService ) { 
 
-    cart.cartList.subscribe((observable: Moto[]) => this.cartList =observable);
- }
+export class MotoCartComponent implements OnInit {
+
+
+  cartList!: Moto[];
+  constructor(private cart: MotoCartService) {
+
+    cart.cartList.subscribe((observable: Moto[]) => this.cartList = observable);
+  }
   ngOnInit(): void {
-   
+
   }
 
-  total():number{
-  let total= 0
- 
-  for (let i in this.cartList) {
-        total+=this.cartList[i].price*this.cartList[i].quantity;
-        
-        
-    }
+  total(): number {
+    let total = 0
+      for (let i in this.cartList) {
+        total += this.cartList[i].price * this.cartList[i].quantity;
+      }
     return total;
   }
+
+  vaciar() {
+    this.cartList = [];
+  }
+
+
+
 }
 
 
